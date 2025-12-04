59f6082ebefd49ae1bdf9c1963f8beb8149e64b4
Alex C. Szatmary <alex.szatmary@gmail.com>
2025-12-04 17:39:07 -0500

    Improve test_02 for part 1

--- day-02/test_02.py
+++ day-02/test_02.py
@@ -1,7 +1,7 @@
 import os
 
 import pytest
-from run_02 import parse, solve_1
+from run_02 import is_invalid, parse, solve_1
 
 
 def load(file_name: str) -> list[str]:
@@ -14,6 +14,13 @@ def load(file_name: str) -> list[str]:
 @pytest.fixture
 def nums_1() -> list[tuple[int, int]]:
     return parse(load("inex-02-1.txt"))
+
+
+def test_is_invalid() -> None:
+    assert is_invalid("1188511885")
+    assert is_invalid("11")
+    assert is_invalid("22")
+    assert not is_invalid("121")
 
 
 def test_solve_1(nums_1: list[tuple[int, int]]) -> None:

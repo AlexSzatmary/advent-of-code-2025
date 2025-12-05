dce0613510a5128db7b425e4906ce6a844c9e304
Alex C. Szatmary <alex.szatmary@gmail.com>
2025-12-05 15:45:18 -0500

    Solve day-04-1

---
+++ day-04/run_04.py
@@ -0,0 +1,65 @@
+import sys
+import timeit
+
+import numpy as np
+
+
+def parse(lines: list[str]) -> np.ndarray:
+    return np.array(
+        [[1 if c == "@" else 0 for c in list(line[:-1])] for line in lines]
+    )
+
+
+def count_adjacent(rolls: np.ndarray, i: int, j: int) -> int:
+    n = 0
+    for di, dj in [
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, 1),
+        (1, 1),
+        (1, 0),
+        (1, -1),
+        (0, -1),
+    ]:
+        i1 = i + di
+        j1 = j + dj
+        if (
+            0 <= i1 <= rolls.shape[0] - 1
+            and 0 <= j1 <= rolls.shape[1] - 1
+            and rolls[i1, j1] == 1
+        ):
+            n += 1
+    return n
+
+
+def solve_1(rolls: np.ndarray) -> int:
+    n = 0
+    for i in range(rolls.shape[0]):
+        for j in range(rolls.shape[1]):
+            if rolls[i, j] and count_adjacent(rolls, i, j) < 4:
+                n += 1
+    return n
+
+
+def main(argv: list[str] | None = None) -> None:
+    if argv is None:
+        argv = sys.argv
+    if argv[0] == "python":
+        argv = argv[1:]
+    with open(argv[-1]) as hin:
+        input_lines = hin.readlines()
+    rolls = parse(input_lines)
+    start = timeit.default_timer()
+    if "1" in argv:
+        print(solve_1(rolls))
+    # if "2" in argv:
+    #     print(solve_2(rolls))
+
+    stop = timeit.default_timer()
+    if "time" in argv:
+        print("Time:", stop - start)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ day-04/test_04.py
@@ -0,0 +1,42 @@
+import os
+
+import numpy as np
+import pytest
+from run_04 import parse, solve_1, count_adjacent
+from inspect import cleandoc
+
+
+def load(file_name: str) -> list[str]:
+    inex_path = os.path.join(os.path.dirname(__file__), file_name)
+    with open(inex_path) as hin:
+        lines = hin.readlines()
+    return lines
+
+
+@pytest.fixture
+def rolls_1() -> np.ndarray:
+    return parse(load("inex-04-1.txt"))
+
+
+def test_parse(rolls_1: np.ndarray) -> None:
+    assert rolls_1.shape == (10, 10)
+
+
+def test_count_adjacent() -> None:
+    rolls = parse(
+        cleandoc(
+            """
+            @@..
+            @@@.
+            @@@.
+            """
+        ).split()
+    )
+    assert count_adjacent(rolls, 0, 0) == 3
+    assert count_adjacent(rolls, 1, 1) == 7
+    assert count_adjacent(rolls, 1, 2) == 4
+    assert count_adjacent(rolls, 2, 1) == 5
+
+
+def test_solve_1(rolls_1: np.ndarray) -> None:
+    assert solve_1(rolls_1) == 13
